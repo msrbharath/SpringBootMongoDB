34ad1c0a0e5ae43714f56b24e49525fb37176767
bharath <msrbharathraj@gmail.com>
2019-04-30 17:20:01 +0530

    Code committed

---
+++ src/main/java/in/bharath/SpringBootMongoDBApplication.java
@@ -0,0 +1,12 @@
+package in.bharath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootMongoDBApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoDBApplication.class, args);
+	}
+}
---
+++ src/main/java/in/bharath/models/CollectionzzRepository.java
@@ -0,0 +1,7 @@
+package in.bharath.models;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CollectionzzRepository extends MongoRepository<Collectionzz, String> {
+
+}
